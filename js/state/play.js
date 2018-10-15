e89c594b86d915c9a413976cf108c824f1253413
Grace <grko@ucsc.edu>
2018-10-15 15:33:46 -0700

    reorganize and rename variables

--- js/state/play.js
+++ js/state/play.js
@@ -46,44 +46,45 @@ play.prototype = {
 
 		// background stars that made the bridge?
 
+		// creating background star group
 		this.bgstar = game.add.group();
 		this.bgstar.physicsBodyType = Phaser.Physics.ARCADE;
 		this.bgstar.enableBody = true;
-		
-		let bgStar = this.bgstar.create(80, 150, 'skystar');
-		bgStar.anchor.setTo(0.5);
 
 		// bgstar to overlap with skystar
-		bgStar2 = this.bgstar.create(160, 150, 'skystar');
-		bgStar2.anchor.setTo(0.5);
+		overlapStar = this.bgstar.create(160, 150, 'skystar');
+		overlapStar.anchor.setTo(0.5);
 		//bgStar2.scale.setTo(2);
-		bgStar2.enableBody = true;
-
-		bgStar = this.bgstar.create(240, 150, 'skystar');
-		bgStar.anchor.setTo(0.5);
+		overlapStar.enableBody = true;
 
 		//bg star to overlap with skystar2
-		bgStar3 = this.bgstar.create(320, 150, 'skystar1');
-		bgStar3.anchor.setTo(0.5);
-		bgStar3.enableBody = true;
+		overlapStar2 = this.bgstar.create(320, 150, 'skystar1');
+		overlapStar2.anchor.setTo(0.5);
+		overlapStar2.enableBody = true;
 		
-		bgStar = this.bgstar.create(400, 150, 'skystar');
+		bgStar = this.bgstar.create(80, 150, 'skystar');
 		bgStar.anchor.setTo(0.5);
+
+		bgStar2 = this.bgstar.create(240, 150, 'skystar');
+		bgStar2.anchor.setTo(0.5);
+		
+		bgStar3 = this.bgstar.create(400, 150, 'skystar');
+		bgStar3.anchor.setTo(0.5);
 		
 		// bg lakestars
 
 		this.bglakestar = game.add.group();
-		let bglakeStar = this.bglakestar.create(80, 450, 'lakestar');
+		bglakeStar = this.bglakestar.create(80, 450, 'lakestar');
 		bglakeStar.anchor.setTo(0.5);
 		bglakeStar.scale.setTo(0.1);
 
-		bglakeStar = this.bglakestar.create(240, 450, 'lakestar');
-		bglakeStar.anchor.setTo(0.5);
-		bglakeStar.scale.setTo(0.1);
+		bglakeStar2 = this.bglakestar.create(240, 450, 'lakestar');
+		bglakeStar2.anchor.setTo(0.5);
+		bglakeStar2.scale.setTo(0.1);
 
-		bglakeStar = this.bglakestar.create(400, 450, 'lakestar');
-		bglakeStar.anchor.setTo(0.5);
-		bglakeStar.scale.setTo(0.1);
+		bglakeStar3 = this.bglakestar.create(400, 450, 'lakestar');
+		bglakeStar3.anchor.setTo(0.5);
+		bglakeStar3.scale.setTo(0.1);
 
 	},
 	
@@ -94,12 +95,12 @@ play.prototype = {
 		// game.physics.arcade.overlap(skystar, this.bgstar, this.bridge1, null, this);
 		// overlap = game.physics.arcade.overlap(skystar2, this.bgstar, this.bridge2, null, this);
 
-		if(checkOverlap(skystar, bgStar2))
+		if(checkOverlap(skystar, overlapStar))
 		{
 			console.log('bridge 1');
 			// call overlap function
 		}
-		if(checkOverlap(skystar2, bgStar3))
+		if(checkOverlap(skystar2, overlapStar2))
 		{
 			console.log('bridge 2');
 			// call overlap function
