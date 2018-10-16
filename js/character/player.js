39cfc91b73d0dc6218fd65b259cd8a6016fccbb6
Planeswalker <allanzhai@gmail.com>
2018-10-15 17:25:25 -0700

    up/down movement(wont work)

--- js/character/player.js
+++ js/character/player.js
@@ -18,14 +18,19 @@ Player.prototype.update = function() {
 	if(game.input.keyboard.isDown(Phaser.Keyboard.D)){
 		this.body.velocity.x = 50;
 		console.log('right');
-	}
-	else if(game.input.keyboard.isDown(Phaser.Keyboard.A)){
+	} else if(game.input.keyboard.isDown(Phaser.Keyboard.A)){
 		this.body.velocity.x = -50;
 		console.log('left');
+	} else if(game.input.keyboard.isDown(Phaser.Keyboard.W)){
+		this.body.velocity.y = -50;
+		console.log('up');
+	} else if(game.input.keyboard.isDown(Phaser.Keyboard.S)){
+		this.body.velocity.y = 50;
+		console.log('down');
 	}else{
 		this.body.velocity.x = 0;
-	} 
-	
+	}
+
 
 
 }
