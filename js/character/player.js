2bf97184741383d9047d7fc9831b1760021768ba
Sung_Baek <shbaek@ucsc.edu>
2018-10-19 02:10:26 -0700

    really fixed jump this time

--- js/character/player.js
+++ js/character/player.js
@@ -28,12 +28,13 @@ Player.prototype.update = function() {
 		} else{
 			this.body.velocity.x = 0;
 		}
-	}
-	if(game.input.keyboard.isDown(Phaser.Keyboard.W)){
-		if(this.body.velocity.y == 0){
+		if(game.input.keyboard.isDown(Phaser.Keyboard.W)){
+			if(this.body.velocity.y == 0){
 			this.body.velocity.y = -80;
+			}
 		}
 	}
+	
 
 
 
