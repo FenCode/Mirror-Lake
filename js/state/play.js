89216fd5d4dade2dbe058960a3ee97544311dc88
Sung_Baek <shbaek@ucsc.edu>
2018-10-24 19:41:09 -0700

    turned on music and fx

--- js/state/play.js
+++ js/state/play.js
@@ -20,7 +20,7 @@ play.prototype = {
 		bounds = new Phaser.Rectangle(0, game.height/2, game.width, 1320);
 		// bg music
 		var musics = game.add.audio('twinkle', 0.5, true);
-		//musics.play();
+		musics.play();
 
 		// adding reset button
 		resetButton = game.add.button(64, 32, 'reset', resetPlay, this);
@@ -251,10 +251,10 @@ function overlap(skystar, lakestar, overlapStar)
 	let replacementlakeStar = this.rlakestar.create(lakestar.x , lakestar.y, 'lakeStar2');
 	replacementlakeStar.anchor.setTo(0.5);
 	replacementlakeStar.scale.setTo(0.7);
-	//sfx
-	// var sfx = game.add.audio('magic', 0.3, false);
-	// sfx.allowMultiple = false;
-	// sfx.play();
+	// sfx
+	var sfx = game.add.audio('magic', 0.3, false);
+	sfx.allowMultiple = false;
+	sfx.play();
 
 	// changing boolean to true so overlap wont repeat
 	skystar.starLocked = true;
