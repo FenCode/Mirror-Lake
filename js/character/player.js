51087267422233b0d25123201f1621f88ae2f8dd
Sung_Baek <shbaek@ucsc.edu>
2018-10-27 11:53:56 -0700

    player movement changed

--- js/character/player.js
+++ js/character/player.js
@@ -22,9 +22,9 @@ Player.prototype.update = function() {
 
 	if(fmove == 0){
 		if(game.input.keyboard.isDown(Phaser.Keyboard.D)){
-			this.body.velocity.x = 150;
+			this.body.velocity.x = 50;
 		} else if(game.input.keyboard.isDown(Phaser.Keyboard.A)){
-			this.body.velocity.x = -150;
+			this.body.velocity.x = -50;
 		} else{
 			this.body.velocity.x = 0;
 		}
@@ -39,7 +39,7 @@ Player.prototype.update = function() {
 		// }
 		if(game.input.keyboard.isDown(Phaser.Keyboard.W)){
 			if(this.body.velocity.y == 0){
-			this.body.velocity.y = -80;
+			this.body.velocity.y = -70;
 			}
 		}
 
